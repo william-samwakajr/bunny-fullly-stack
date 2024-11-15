e65c221e2795085490d292e73c42236ea41cbe94
william-samwakajr <141919554+william-samwakajr@users.noreply.github.com>
2024-11-15 12:55:37 +0200

    almost there boys

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
+require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = 3000;
 
 app.use(express.static('public'));
 
 
-app.listen(port,()=>{
-    console.log(`listening on port 3000`);
+app.listen(process.env.port,()=>{
+    console.log(`listening on port ${process.env.port}`);
 })
