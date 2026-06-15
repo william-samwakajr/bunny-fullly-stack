a410b749350fd0a880711cfc653f1c24fb9000f9
william samwaka <williamsamwaka05@gmail.com>
2026-06-15 18:10:01 +0200

    restart game with tap

--- public/src/scenes/gameo.js
+++ public/src/scenes/gameo.js
@@ -15,11 +15,14 @@ export default class GameOver extends Phaser.Scene {
         this.add.text(width*0.5,height*0.5,'Game Over',{color:'#000',fontSize: 48})
         .setOrigin(0.5)
 
-        this.add.text(width*0.5, height*0.6, 'Press SPACE to try again', {color:'#000',fontSize:30})
+        this.add.text(width*0.5, height*0.6, 'Press SPACE  or tap screen to try again', {color:'#000',fontSize:30})
         .setOrigin(0.5)
 
         this.input.keyboard.once('keydown-SPACE',()=>{
            this.scene.start('game')
        })
+       this.input.on('pointerdown',(pointer)=>{
+            this.scene.start('game');
+       });
     }
 }
